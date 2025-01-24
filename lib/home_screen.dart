cfd91cdd42ff4b028679eceb8d0c02680ee46e04
yara30999 <yaranaserelniny@gmail.com>
2025-01-24 16:01:16 +0200

    Combine BlocListener and BlocBuilder in a single widget using BlocConsume

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -13,63 +13,61 @@ class HomeScreen extends StatelessWidget {
         backgroundColor: Colors.grey,
         title: const Text('Counter screen'),
       ),
-      body: BlocListener<CounterBloc, CounterState>(
-        listener: (context, state) {
-          if (state.counter == 7 || state.counter == -7) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text('Counter reached ${state.counter}  :)'),
-              ),
-            );
-          }
-        },
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            BlocBuilder<CounterBloc, CounterState>(
-              builder: (context, state) {
-                return Text(
-                  'Counter Value: ${state.counter}',
-                  style: const TextStyle(fontSize: 24),
-                );
-              },
-            ),
-            const SizedBox(height: 60),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                IconButton.filled(
-                    onPressed: () {
-                      context.read<CounterBloc>().add(IncrementCounter());
-                    },
-                    icon: Icon(Icons.add)),
-                IconButton.filled(
-                    onPressed: () {
-                      context.read<CounterBloc>().add(DecrementCounter());
-                    },
-                    icon: Icon(Icons.remove)),
-              ],
-            ),
-            const SizedBox(height: 40),
-            BlocConsumer<ThemeBloc, ThemeState>(
-              listener: (context, state) {
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BlocConsumer<CounterBloc, CounterState>(
+            listener: (context, state) {
+              if (state.counter == 7 || state.counter == -7) {
                 ScaffoldMessenger.of(context).showSnackBar(
                   SnackBar(
-                    content: Text('${state.themeData.toString()} turned on :)'),
+                    content: Text('Counter reached ${state.counter}  :)'),
                   ),
                 );
-              },
-              builder: (context, state) {
-                return ElevatedButton(
+              }
+            },
+            builder: (context, state) {
+              return Text(
+                'Counter Value: ${state.counter}',
+                style: const TextStyle(fontSize: 24),
+              );
+            },
+          ),
+          const SizedBox(height: 60),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton.filled(
                   onPressed: () {
-                    context.read<ThemeBloc>().add(ToggleTheme());
+                    context.read<CounterBloc>().add(IncrementCounter());
                   },
-                  child: Text('Toggle Theme'),
-                );
-              },
-            ),
-          ],
-        ),
+                  icon: Icon(Icons.add)),
+              IconButton.filled(
+                  onPressed: () {
+                    context.read<CounterBloc>().add(DecrementCounter());
+                  },
+                  icon: Icon(Icons.remove)),
+            ],
+          ),
+          const SizedBox(height: 40),
+          BlocConsumer<ThemeBloc, ThemeState>(
+            listener: (context, state) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text('${state.themeData.toString()} turned on :)'),
+                ),
+              );
+            },
+            builder: (context, state) {
+              return ElevatedButton(
+                onPressed: () {
+                  context.read<ThemeBloc>().add(ToggleTheme());
+                },
+                child: Text('Toggle Theme'),
+              );
+            },
+          ),
+        ],
       ),
     );
   }
